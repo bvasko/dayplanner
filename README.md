b5fa2d54970832e538a514d81f9f9758dc832536
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-15 15:26:47 -0500

    initial commit; renders the unstyled rows to the page

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dayplanner front-end# 05 Third-Party APIs: Work Day Scheduler
+
+## About
+
+Show the hours in the workday colorcoded for past present future.
+Allows engineers to track time spent in meetings, fixing bugs, reviewing PRs and writing new features
+
+
+## Usage
+
+Click in a row to save the input
+
+The following animation demonstrates the application functionality:
+
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+
---
+++ js/main.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+  // display current timestamp
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+  const hoursInDay = 9;
+  const startTime = 8;
+  /**
+   * stub functions to get/set localStorage
+   */
+  const saveInput = () => {};
+  const displayInput = () => {};
+  
+  /**
+   * 
+   * @param {number} hour
+   * @returns string
+   */
+  const getColorCode = (hour) => {
+    const t = new Date();
+    return (t.getHours() === hour) ? 'present' : (hour < t.getHours()) ? 'past' : 'future';
+  };
+
+  /**
+   * 
+   * return row element to be rendered
+   */
+  const renderRow = (h, color) => {
+    return `
+      <div class="row">
+        <div class="col-2">${h}</div>
+        <div class="col-8 ${color}"><input /></div>
+        <div class="col-2"><button class="save">Save</button></div>
+      </div>
+    `;
+  };
+  /**
+   * 
+   * render all the rows
+   */
+  const renderRows = () => {
+    let container = $(".container");
+    for (let i = 0; i < hoursInDay; i++) {
+      const rowHour = startTime + i;
+      /* pass in hour */
+      const rowColor = getColorCode(rowHour);
+      const r = renderRow(rowHour, rowColor, i);
+      container.append(r);
+    }
+  };
+  renderRows();
+});
+/**
+ * color code for past: #eee, present: blue, future: greenish
+ */
